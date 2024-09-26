869b46e1206e93e4a0685176254940c1f6d46403
arnieS06 <sharma.arnav2016@gmail.com>
2024-09-25 21:51:07 -0700

    thought out the functions

--- mainfile.cpp
+++ mainfile.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
-#include 
+#include <cstlib>
+#include <ctime>
+#include <string>
 using namespace std;
 
-// blackjack
+
+// functiton makes the random card numbers from 1 - 13
+int drawHand(string playerType) {
+    int firstCard = (rand() % 13) + 1;
+    int secondCard = (rand() % 13) + 1;
+
+    
+}
+
+string cardToString(int card) {
+// thinking of using switch statement so far
+
+}
+// this function ill use to assign card values
+// some cards are worth a letter
+int cardValue(int card) {
+
+
+}
+
+void findWinner(int playerSum, int dealerSum, string playerName) {
+    //using this function to find final winner i dont need a return type yet
+}
 
 int main() {
 
+    // ask for input 
+    // draw hand
+    // use card value and assign values to all drawn cards
+    // see if cards could have special values like letters
+    // compare sum of cards
+    // find the winner for that round
+    // ask them to play again?
+    // break the program if they say no
 }
+
