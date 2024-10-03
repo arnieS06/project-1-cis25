d7119ce2bf7e5244e128528b08e70d360dbe94fb
arnieS06 <sharma.arnav2016@gmail.com>
2024-10-02 20:19:00 -0700

    fixed line flushing and added comments

--- mainfile.cpp
+++ mainfile.cpp
@@ -2,13 +2,15 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <limits>
 using namespace std;
-
 
 string cardToString(int card);
 int cardValue(int card);
 
-// functiton makes the random card numbers from 1 - 13
+// this functiton draws random card numbers from 1 - 13,
+// prints the first and second card,
+// and gives a sum of the player's cards
 int drawHand(string playerType) {
     int firstCard = (rand() % 13) + 1;
     int secondCard = (rand() % 13) + 1;
@@ -19,6 +21,9 @@ int drawHand(string playerType) {
     return cardValue(firstCard) + cardValue(secondCard);
 }
 
+
+// this function is for special cases where some 
+// numbered cards have special values like "ace", etc.
 string cardToString(int card) {
     switch (card) {
         case 1: 
@@ -33,8 +38,9 @@ string cardToString(int card) {
             return to_string(card);
     }
 }
-// this function ill use to assign card values
-// some cards are worth a letter
+
+// this function assigns specific numerical values
+// to cards that exceet a certain limit
 int cardValue(int card) {
     if (card == 1) {
         return 11;
@@ -50,6 +56,8 @@ int cardValue(int card) {
 
 }
 
+// this function uses logic to find if the
+// player or dealer won the round 
 void findWinner(int playerSum, int dealerSum, string playerName, bool &playerWin) {
     //using this function to find final winner 
     cout << "\nSum of player's cards: " << playerSum << endl;
@@ -64,24 +72,33 @@ void findWinner(int playerSum, int dealerSum, string playerName, bool &playerWin
         cout << "Dealer wins!" << endl;
     }
 
+    else if (playerSum > 21) {
+        cout << "It looks like your hand exceeds 21, you lose. bad luck." << endl;
+    }
+
     else {
         cout << "Draw, no one wins." << endl;
     }
 }
 
+
+// this function finds out if the player wins
+// or loses money based on their match outcome
 void betWinner(int playerBet, bool playerWin) {
     if (playerWin) {
-        cout << "Looks like the dealer owes you your money,\n";
+        cout << "\nLooks like the dealer owes you your money,\n";
         cout << "go and get the " << playerBet << "$ you won!" << endl;
     }
 
     else {
-        cout << "Seems like you won no money,\n";
+        cout << "\nSeems like you won no money,\n";
         cout << "you must've drawn or lost the round.\n";
         cout << "Time to pay the dealer " << playerBet << "$" << endl;
     }
 }
 
+
+// calling all functions and game logic
 int main() {
     
     srand(time(0));
@@ -98,28 +115,30 @@ int main() {
 
         cout << "Welcome to blackjack, please enter your name -> ";
         getline(cin, player);
-        cin.ignore();
 
         cout << "How many $ do you wanna bet that you'll win this round? -> ";
         cin >> playerBet;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         cout << "\n" << player << "'s hand: \n";
         int playerSum = drawHand(player);
+
+
+        cout << "\nDealer's hand: \n";
+        int dealerSum = drawHand("Dealer");
+ 
+
+        // finding the winner of the match
+        findWinner(playerSum, dealerSum, player, playerWin);
 
         if (playerSum == 21) {
             cout << "\n Blackjack!!";
             playerWin = true;
         }
 
-        cout << "\nDealer's hand: \n";
-        int dealerSum = drawHand("Dealer");
-
         if (dealerSum == 21) {
             cout << "Dealer has gotten blackjack!\n";
-        } 
-
-        // finding the winner of the match
-        findWinner(playerSum, dealerSum, player, playerWin);
+        }
 
 
         // finding the winner of the bet the player placed
@@ -128,6 +147,7 @@ int main() {
         // asking player if they wanna play again
         cout << "\n Wanna play one more time? (Y/N): ";
         getline(cin, playerResponse);
+        
         if (playerResponse != "Y" && playerResponse != "y") {
             playAgain = false;
             cout << "come play again next time!" << endl;
@@ -137,4 +157,3 @@ int main() {
 
     
 }
-
